c725f7d9ac418a52e3951bc17c815333a45aeaf0
Alessandro Delgado <adelgado1313@gmail.com>
2016-03-06 19:01:28 +0100

    Add /Native to linted files

--- Native/Renderer.js
+++ Native/Renderer.js
@@ -1,14 +1,15 @@
-var make = function make(localRuntime) {
-    localRuntime.Native = localRuntime.Native || {};
-    localRuntime.Native.Renderer = localRuntime.Native.Renderer || {};
+/* eslint no-undef: 0 */
+const make = function make(localRuntime) {
+	localRuntime.Native = localRuntime.Native || {}
+	localRuntime.Native.Renderer = localRuntime.Native.Renderer || {}
 
-    var toHtml = require('vdom-to-html');
+	const toHtml = require('vdom-to-html')
 
-    return {
-        'toHtml': toHtml
-    };
-};
+	return {
+		'toHtml': toHtml
+	}
+}
 
-Elm.Native = Elm.Native || {};
-Elm.Native.Renderer = Elm.Native.Renderer || {};
-Elm.Native.Renderer.make = make;
+Elm.Native = Elm.Native || {}
+Elm.Native.Renderer = Elm.Native.Renderer || {}
+Elm.Native.Renderer.make = make
