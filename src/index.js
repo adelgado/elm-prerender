2db78435372734ee9bee6613b8d82689fcd62626
Alessandro Delgado <adelgado1313@gmail.com>
2016-03-16 22:50:05 +0100

    Rename files to make more sense

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import commandLineArgs      from 'command-line-args'
+import getUsage             from 'command-line-usage'
+import tool                 from 'command-line-tool'
+
+import main from './main'
+
+const cliDefinitions = [
+	{ name: 'input-folder'
+	, alias: 'i'
+	, typeLabel: 'folder'
+	, description: 'Path to the input files'
+	, type: String
+	}
+,
+	{ name: 'output-folder'
+	, alias: 'o'
+	, typeLabel: 'folder'
+	, description: 'Where to write the output files'
+	, type: String
+	}
+,
+	{ name: 'help'
+	, alias: 'h'
+	, description: 'Displays this usage text'
+	, type: Boolean
+	}
+]
+
+const cli = commandLineArgs(cliDefinitions)
+const options = cli.parse()
+
+if (options.help) {
+	const usage = getUsage(cliDefinitions, options)
+	console.log(usage)
+	tool.stop()
+} else if (typeof options['input-folder'] == 'undefined') {
+	console.log('Option --input-folder is not optional')
+	tool.stop()
+} else if (typeof options['output-folder'] == 'undefined') {
+	console.log('Option --output-folder is not optional')
+	tool.stop()
+} else {
+	main(options['input-folder'], options['output-folder'])
+}
--- src/main.js
+++ src/main.js
@@ -1,44 +1,106 @@
-import commandLineArgs      from 'command-line-args'
-import getUsage             from 'command-line-usage'
-import tool                 from 'command-line-tool'
+import fs                            from 'fs'
+import spawn                         from 'cross-spawn'
+import { find, grep }                from 'shelljs'
+import compiler                      from 'node-elm-compiler'
 
-import main from './convert'
+import portTemplate                  from './templates/port'
+import mappingTemplate               from './templates/mapping'
+import rendererTemplate              from './templates/renderer'
+import importTemplate                from './templates/import'
 
-const cliDefinitions = [
-	{ name: 'input-folder'
-	, alias: 'i'
-	, typeLabel: 'folder'
-	, description: 'Path to the input files'
-	, type: String
-	}
-,
-	{ name: 'output-folder'
-	, alias: 'o'
-	, typeLabel: 'folder'
-	, description: 'Where to write the output files'
-	, type: String
-	}
-,
-	{ name: 'help'
-	, alias: 'h'
-	, description: 'Displays this usage text'
-	, type: Boolean
-	}
-]
+import helpers                       from './helpers'
 
-const cli = commandLineArgs(cliDefinitions)
-const options = cli.parse()
-
-if (options.help) {
-	const usage = getUsage(cliDefinitions, options)
-	console.log(usage)
-	tool.stop()
-} else if (typeof options['input-folder'] == 'undefined') {
-	console.log('Option --input-folder is not optional')
-	tool.stop()
-} else if (typeof options['output-folder'] == 'undefined') {
-	console.log('Option --output-folder is not optional')
-	tool.stop()
-} else {
-	main(options['input-folder'], options['output-folder'])
+function listFiles(path) {
+	return (find(path)
+		.filter(file => file.match(/\.elm$/))
+		.filter(file => grep('view =', file) !== '')
+		.map(file => file.replace(path, ''))
+	)
 }
+
+function generatePort(moduleName) {
+	const portName = helpers.moduleToPortName(moduleName)
+	return portTemplate({portName, moduleName})
+}
+
+function generateImport(moduleName) {
+	return importTemplate({moduleName})
+}
+
+function fileName(moduleName, basedir = './', withLower) {
+	const withBasedir = basedir + moduleName.replace('.','/')
+
+	if (withLower) {
+		return withBasedir.toLowerCase()
+	}
+
+	return withBasedir
+}
+
+function generateMapping(port, file) {
+	return mappingTemplate({port, file})
+}
+
+function generateVDom(moduleNames, basedir) {
+	const portFiles = []
+
+	for (let i = moduleNames.length - 1; i >= 0; i--) {
+		portFiles.push({
+			'port' : helpers.moduleToPortName(moduleNames[i]),
+			'filename' : fileName(moduleNames[i], basedir, false)
+		})
+	}
+
+	const ports = moduleNames.map(generatePort).join('\n')
+	const imports = moduleNames.map(generateImport).join('\n')
+
+	const portFileValues = portFiles.map(curr => curr.filename)
+
+	const maps = portFiles.map(curr =>
+		generateMapping(curr.port, curr.filename)
+	)
+
+	const fileMappings = maps.join('\n')
+
+	makeFolders(portFileValues)
+
+	const rendererFilename = '_Renderer.elm'
+
+	const template = rendererTemplate({imports, ports})
+
+	fs.writeFile(rendererFilename, template)
+
+	compiler.compileToString([rendererFilename], { yes: true })
+		.then(data => {
+			data += "\nvar fs = require('fs');"
+			data += "\nvar elm = Elm.worker(Elm.Renderer);"
+			data += `\n${fileMappings}`
+			fs.writeFileSync('./_main.js', data)
+			spawn('node', ['./_main.js'], { stdio: 'inherit' })
+		})
+}
+
+function makeFolders(filenames) {
+	filenames.forEach(function(filename) {
+		if (filename.split('/').length > 1 || filename.startsWith('.') != -1) {
+			const dir = filename.substring(0, filename.lastIndexOf('/'))
+			try {
+				fs.mkdirSync(dir)
+			} catch (e) {
+				if (e.code === 'EEXIST') {
+					console.warn(`${dir} already exists`)
+				} else {
+					throw e
+				}
+			}
+		}
+	})
+}
+
+function main(inputFolder, outputFolder) {
+	const modules = listFiles(inputFolder).map(helpers.modulifyPath)
+
+	generateVDom(modules, outputFolder)
+}
+
+export default main
